5da962eeb22ab44e97994bdf061ad8324c7518ee
Akshar Patel <akshar8460@gmail.com>
2023-08-22 17:08:20 -0400

    Login API Integration Successful

--- blogging-platform-fe/src/App.js
+++ blogging-platform-fe/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import DataDisplay from './DataDisplay';
+import LoginForm from './LoginForm';
 
 function App() {
     return (
         <div className="App">
-            <DataDisplay />
+            <LoginForm/>
         </div>
     );
 }
---
+++ blogging-platform-fe/src/LoginForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/users/login', {email, password});
+            setMessage(response.data.message);
+        } catch (error) {
+            setMessage('An error occurred during login.');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default LoginForm;
