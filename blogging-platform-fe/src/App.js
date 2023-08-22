20d7a21b754c9a7e63a43bbe51051a3a0b963219
yours-truly-rshi <96832952+yours-truly-rshi@users.noreply.github.com>
2023-08-22 15:27:10 -0400

    Initial Commit

---
+++ blogging-platform-fe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataDisplay from './DataDisplay';
+
+function App() {
+    return (
+        <div className="App">
+            <DataDisplay />
+        </div>
+    );
+}
+
+export default App;
