da96f55c8b181f09d64a26da4833326d14c45294
Akshar Patel <akshar8460@gmail.com>
2023-08-26 15:25:44 -0400

    Update Profile Implemented

--- blogging-platform-fe/src/Profile.js
+++ blogging-platform-fe/src/Profile.js
@@ -1,16 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {axiosInstance} from "./utils";
 import jwt_decode from 'jwt-decode';
+import ProfileUpdate from "./ProfileUpdate";
+
 
 const Profile = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isUpdatePopupOpen, setIsUpdatePopupOpen] = useState(false);
+
+    const openUpdatePopup = () => {
+        setIsUpdatePopupOpen(true);
+    };
+
+    const closeUpdatePopup = () => {
+        setIsUpdatePopupOpen(false);
+    };
+
     useEffect(() => {
         const token = localStorage.getItem('jwtToken');
         const decodedToken = jwt_decode(token);
         const id = decodedToken.sub;
+        setID(id);
         axiosInstance.get(`/api/users/${id}`).then(response => {
             setName(response.data.name)
             setPassword(response.data.password)
@@ -20,12 +33,16 @@ const Profile = () => {
         })
     }, []);
 
-
     return (
         <div>
             <h1>Name : {name}</h1>
             <h1>Email : {email}</h1>
             <h1>Password : {password}</h1>
+            <button onClick={openUpdatePopup}>
+                Update Profile
+            </button>
+            <ProfileUpdate isOpen={isUpdatePopupOpen} onRequestClose={closeUpdatePopup} name={name} email={email}
+                           password={password}/>
         </div>
     )
 }
